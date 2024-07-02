bb270c4575ba9066afdede46fe981cc4c38fdd85
Shimar-Ahamed <shimar10106@gmail.com>
2024-07-02 17:48:03 +0530

    added git hub link for projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,30 +12,35 @@ const Projects = () => {
         <img src={pythonLogo} alt="Python" className="logo" />
         <h2>Progression Outcome Predictor (Console)</h2>
         <p>Designed and implemented a Python application to accurately predict students' academic progression outcomes. - 2022</p>
+        <a href="https://github.com/Shimar-Ahamed/Progression-Outcome-Predictor-Console-" target="_blank" rel="noopener noreferrer">View on GitHub</a>
       </div>
 
       <div className="project">
         <img src={htmlCssJsLogo} alt="HTML/CSS/JavaScript" className="logo" />
         <h2>Exploring the Mysteries of the Universe (Group)</h2>
         <p>A website including a quiz, feedback form, shopping cart, and sitemap - 2023</p>
+        
       </div>
 
       <div className="project">
         <img src={javaLogo} alt="Java" className="logo" />
         <h2>Theater Program</h2>
         <p>A program to manage the ticketing process of theater using CRUD operations. - 2023</p>
+        <a href="https://github.com/Shimar-Ahamed/Theater-Management-System" target="_blank" rel="noopener noreferrer">View on GitHub</a>
       </div>
 
       <div className="project">
         <img src={javaLogo} alt="Java" className="logo" />
         <h2>Westminster Shopping System</h2>
         <p>A shopping system developed using Java, Swing, and OOP concepts - 2023-2024</p>
+        <a href="https://github.com/Shimar-Ahamed/Westminster-Shopping-System" target="_blank" rel="noopener noreferrer">View on GitHub</a>
       </div>
 
       <div className="project">
         <img src={mernLogo} alt="MERN" className="logo" />
         <h2>Personal Portfolio Website</h2>
         <p>My personal portfolio website created with the MERN stack. - 2023-2024</p>
+        <a href="https://github.com/aaqilruzzan/RemEz" target="_blank" rel="noopener noreferrer">View on GitHub</a>
       </div>
     </div>
   );
